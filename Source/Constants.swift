e92aaa82cc6db69795d80589dfbd0244a07ce979
John Busby <johncbusby@gmail.com>
2022-02-22 09:07:32 -0800

    reduce standard row height

--- Source/Constants.swift
+++ Source/Constants.swift
@@ -11,5 +11,5 @@ import UIKit
 internal struct Constants {
     internal static let TEXT_FIELD_HSPACE: CGFloat = 6.0
     internal static let MINIMUM_TEXTFIELD_WIDTH: CGFloat = 56.0
-    internal static let STANDARD_ROW_HEIGHT: CGFloat = 35.0
+    internal static let STANDARD_ROW_HEIGHT: CGFloat = 28.0
 }
